8194082ab80ff0313c5fc318a4f8985ad3238114
gtgunar <120199255+gtgunar@users.noreply.github.com>
2023-04-23 15:14:00 +0200

    InnerData finished
    improved inner data upload

--- accarr_innerData.h
+++ accarr_innerData.h
@@ -4,7 +4,7 @@
 #include<iostream>
 using namespace std;
 
-unsigned SQRT(unsigned x)
+unsigned SQRT(unsigned x)//integer square root
     {
     unsigned toret=1;
     unsigned mask=0x80000000;
@@ -13,9 +13,8 @@ unsigned SQRT(unsigned x)
         mask/=2;
         if(((toret+mask)*(toret+mask))<=x)
             {toret=toret+mask;}
-        
         }
-    return toret;
+    return toret*(x>0);
     }
 
 class accarr_innerData
@@ -25,73 +24,113 @@ class accarr_innerData
     int popextra;//current value of extra population, population-popsn
     int extraCapacity;//2*n
     pair<int,int> hotPlace;
+    pair<int,int> coldPlace;
     bool endLoad;//is the end the loadingplace
 
     public:
+
+    int getPop()const
+        {return population;}
+
+    pair <int,int> getHotPlace()const
+        {return hotPlace;}
+
+    pair<int,int>getRelPos(int index)const//the pair of block-index and the within block index for a given index value
+        {
+        if(endLoad)
+            {return pair<int,int>(index/popsqrt,index%popsqrt);}
+        else
+            {
+            if((popextra-popsqrt)<=index/(popsqrt+1))//within the incremented block
+                {return pair<int,int>(index/(popsqrt+1),index%(popsqrt+1));}
+            else
+                {
+                int uninced=index-(popextra-popsqrt)*(popsqrt+1);//remaining population stored in nonincremented zone
+                return pair<int,int>(popextra-popsqrt + uninced/popsqrt , uninced%popsqrt);//inc-ed length+rest
+                }
+            }
+        
+        
+        }
+
+    void recalculate()
+        {
+        popsqrt=SQRT(population);
+        popextra=population-popsqrt*popsqrt;
+        extraCapacity=2*popsqrt;
+        endLoad=popextra<=popsqrt;
+        if(endLoad)
+            {hotPlace=pair<int,int>(popsqrt+1,popextra);}
+        else
+            {hotPlace=pair<int,int>(popextra-popsqrt,popsqrt+1);}      
+        }
+
+    accarr_innerData(int pop)
+        {
+        population=pop-1;
+        recalculate();
+        coldPlace=hotPlace;
+        population=pop;
+        recalculate();
+        }
+
+    void incPop()//assumed to be called from insert
+        {
+        coldPlace=hotPlace;
+        population++;
+        recalculate();
+        }
+
+    void decPop()//assumed to be called from delete
+        {
+        if(population>0)
+            {
+            population--;
+            recalculate();
+            population--;
+            recalculate();
+            coldPlace=hotPlace;
+            population++;
+            }
+        }
+
     void logMe()const
         {
         cout<<"population: "<<population<<endl<<
         "popsqrt: "<<popsqrt<<endl<<
         "popextra: "<<popextra<<endl<<
         "extraCapacity: "<<extraCapacity<<endl<<
-        "hotPlace1: "<<hotPlace.first<<endl<<
-        "hotPlace2: "<<hotPlace.second<<endl<<
+        "hotPlaceBlock: "<<hotPlace.first<<endl<<
+        "hotPlaceInner: "<<hotPlace.second<<endl<<
+        "coldPlaceBlock: "<<coldPlace.first<<endl<<
+        "coldPlaceInner: "<<coldPlace.second<<endl<<
         "endload?:"<<endLoad<<endl;
         }
-    int getPop()const{return population;}
-    accarr_innerData(int pop)
-        {
-        population=pop;
-        popsqrt=SQRT(pop);
-        popextra=pop-popsqrt*popsqrt;
-        extraCapacity=population-popsqrt*popsqrt;
-        
-        endLoad=popextra<popsqrt;
-        }
-    pair <int,int> getHotPlace()const{return hotPlace;}
-    pair<int,int>getRelPos(int index)const//the pair of block-index and the within block index for a given index value
-        {
-        return pair<int,int>(0,0);
-        }
-    void incPop()//assumed to be called from insert
-        {
-        population++;
-        if(popextra!=extraCapacity)//still has room
-            {
-            popextra++;
-            if(endLoad)
-                {hotPlace.second++;}
-            else
-                {hotPlace.first++;}
-            if(popextra==popsqrt)
-                {
-                hotPlace.first=0;
-                hotPlace.second++;
-                }
-            endLoad=popextra<popsqrt;
-            }
-        else//new square reached
-            {
-            popsqrt++;
-            popextra=0;
-            extraCapacity+=2;
-            }
-        
-        }
-    void decPop()//assumed to be called from delete
-        {
-        
-        }
+
     };
+
+
+
+
+
     void teszting()
         {
-        accarr_innerData alany(20);
+        accarr_innerData alany(0);
         alany.logMe();
-        for(int i=0;i<10;i++)
+        for(int i=0;i<18;i++)
             {
             cout<<"/////////////"<<endl;
             alany.incPop();
             alany.logMe();
+            //alany.decPop();
+            cout<<"/////////////"<<endl;
+            }
+        for(int i=0;i<18;i++)
+            {
+            cout<<"/////////////"<<endl;
+            //alany.incPop();
+            alany.logMe();
+            alany.decPop();
             cout<<"/////////////"<<endl;
             }
         }
