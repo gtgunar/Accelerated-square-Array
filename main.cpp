f9eb7be8ed1d186e04bc429d0363d0080ff88d43
gtgunar <120199255+gtgunar@users.noreply.github.com>
2023-06-05 13:58:09 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 //Márton Attila, 2023
-//implementation is in "accarr.h" 
 //this file:primarily for testing
 #include<iostream>
 #include<cstdlib>
@@ -19,15 +18,12 @@ using  std::pair;
 using  std::max;
 using  std::string;
 using  namespace std::chrono;
-using namespace StationaryQueue;//////////////////////////////////////////////////////////////////////////////////////////
+using namespace StationaryQueue;
 template<typename T>
 void doTask(vector<T>& controlSample,accarr<T>& tesztTarget,int task,int value,T value2)
     {
     if((controlSample.size()>value)||((value==0)&&(task==1)))//invalid jobs skipped
         {
-      //  cout<<"task:"<<task<<":"<<value<<", "<<value2<<endl;
-      //  cout<<"meret"<<tesztTarget.getPop()<<endl;
-      //  cout<<"task started"<<endl;
         switch(1)
             {
             case 0:{  
@@ -46,16 +42,8 @@ void doTask(vector<T>& controlSample,accarr<T>& tesztTarget,int task,int value,T
                     }
             }break;//delete
             }
-       // cout<<"outcome checked------------------------                         --------------------------------"<<endl;
         isSame(controlSample,tesztTarget);
-       // cout<<"task finished------------------------------------------------------------------------------------"<<endl;
-       // colog(controlSample,tesztTarget);
-      //  tesztTarget.logRaw();
-       // for(auto i: controlSample){cout<<i<<", ";}
-       // cout<<endl;
         string a;
-        //cin>>a;
-
         }
     
     }
@@ -90,8 +78,7 @@ template<typename T>
 void bruteTest(vector<T>& a,accarr<T>& b,ofstream& logFile){
     vector<string> szavak={"asd","qwe","asd2","qwasde","asqwed","qwqwre","ashd","qwfgk","astzud","qw4567e","adfgd","qwfffe","asdwer","qw4z8e","asfgfbd","qwse","artxthsd","qwcxvbne",};
     for(int i=0;i<100;i++)
-        {doTask(a,b,rand()%3,rand()%max(1,(int)a.size()),0.5f+(float)(rand()%20));}/////////////////////////////////
-    //cout<<"isSame chjecked"<<endl;
+        {doTask(a,b,rand()%3,rand()%max(1,(int)a.size()),0.5f+(float)(rand()%20));}
     if(isSame(a,b))
         {
         cout<<".";
@@ -99,38 +86,7 @@ void bruteTest(vector<T>& a,accarr<T>& b,ofstream& logFile){
     else{cout<<"\n!!!\n"<<endl;}
     }
 int main()
-    {   /*
-   SQ<float> a;
-    a.addTo(0,7.3);
-    //a.log();
-    a.addTo(0,1.3);
-    //a.log();
-    a.addTo(1,10.3);
-    //a.log();
-    //1.3, 10.3, 7.3
-    a.push_back(0.1);
-   
-    a.push_front(44.4);
-    a.log();
-    cout<<a.placing(0.001)<<endl;a.log();
-    //cout<<a.revplace(0.1001)<<endl;a.log();
-cout<<a.placing(0.201)<<endl;a.log();
-cout<<a.placing(0.301)<<endl;a.log();
-cout<<a.placing(0.4001)<<endl;a.log();
-cout<<a.placing(0.501)<<endl;
-a.log();
-a.addTo(5,3.3);
-a.log();
-a.addTo(0,30.3);
-a.log();
-a.deleteFrom(1);
-a.log();
-cout<<endl<<a.data->size()<<","<<a.offset<<endl;
-    for(int i=0;i<=a.data->size();i++)
-        {
-        //cout<<i<<":"<<a.getShiftedPos(i)<<", "<<endl;
-        }
-    */
+    {  
     ofstream x("rawout.txt");
     vector<float>a;
     accarr<float>b;
@@ -139,13 +95,3 @@ cout<<endl<<a.data->size()<<","<<a.offset<<endl;
     cout<<"DONE"<<endl;
     return 0;
     }
-
-/*crashes:
-értékadás számból stringbe convertálásnál
-shifted place=actual place
-todo:getshiftedPos
-balanceshift to empty SQ
-1 0 0 task
-erase
-memleak from never dleteing empty SQs
-*/
